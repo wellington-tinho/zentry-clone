b6050d3a214e53eae4f133d5f28d0e1dc89d41eb
Wellington <sousa_wellington@outlook.com>
2025-07-11 16:32:35 -0300

    feat: update navbar layout and button styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,34 +3,54 @@ import { FaLocationArrow } from "react-icons/fa";
 import { GiSoundWaves } from "react-icons/gi";
 
 
+const navBarButtons = [
+  {name: "nexus", hasIcon: true },
+  {name: "vault", hasIcon: true },
+  {name: "prologue" },
+  {name: "about" },
+  {name: "contact" },
+]
+
+const mainBarButtons = [
+  {name: "products", hasIcon: true },
+  {name: "whitepaper" },
+]
 
 export function NavBar(){
+
+
   return (
     <header>
-        <nav className="bg-zinc-900 border border-zinc-800 flex justify-between mx-2 rounded-md">
-          <div className="flex gap-2 items-center">
+        <nav className="fixed inset-x-0 bg-zinc-900 border border-zinc-800 flex justify-between mx-2 rounded-md h-16">
+
+          <div className="flex gap-4 items-center pl-2">
             <a className="cursor-pointer" href="/">
-              <img className="w-12" src="src/assets/zentry-symbol-white.png" alt="Logo zentry" />
+              <img className="w-14" src="src/assets/zentry-symbol-white.png" alt="Logo zentry" />
             </a>
-            <Button>
-              <span className="font-semibold text-xs uppercase">Products</span>
-              <FaLocationArrow className="rotate-134 w-3" />
-            </Button>
-            <Button>
-               <span className="font-semibold text-xs uppercase">whitepaper</span>
-            </Button>
+
+            <div className="flex gap-3">
+              {
+                mainBarButtons.map((item)=>(
+                <Button>
+                  <span className="font-bold text-[12px] uppercase">{item.name}</span>
+                  {item.hasIcon && <FaLocationArrow className="rotate-135 w-2 mb-[1px]" />}
+                </Button>
+                ))
+              } 
+            </div>
           </div>
 
-          <div className="flex gap-10 items-center px-8">
-            <Button variant="navbar"><span className="font-semibold text-xs uppercase">nexus</span></Button>
-            <Button variant="navbar"><span className="font-semibold text-xs uppercase">valt</span></Button>
-            <Button variant="navbar"><span className="font-semibold text-xs uppercase">prolog</span></Button>
-            <Button variant="navbar"><span className="font-semibold text-xs uppercase">about</span></Button>
-            <Button variant="navbar"><span className="font-semibold text-xs uppercase">contact</span></Button>
-            <GiSoundWaves className="w-6 h-6 mt-[2px] text-white"/>
-            
+          <div className="flex gap-4 items-center px-8">
+            {
+              navBarButtons.map((item)=>(
+                <Button variant="navbar" className="flex gap-2 items-center">
+                  <span className="font-bold text-[12px] uppercase">{item.name}</span>
+                  {item.hasIcon && <FaLocationArrow className="w-2"/>}
+                </Button>
+              ))
+            }
+            <GiSoundWaves className="flex cursor-pointer items-center w-6 h-6 mt-[2px] text-white"/>
           </div>
-
 
         </nav>
     </header>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,16 +9,16 @@ interface IPropsButton {
 
 
 const variantButton = {
-  "default": "w-fit flex gap-2 items-center py-2 px-3 cursor-pointer rounded-full bg-violet-50 text-black",
+  "default": "transition-all ease-in-out w-fit flex gap-2 items-center py-2 px-3 cursor-pointer rounded-full bg-violet-50 text-black hover:rounded-lg",
   "main": "w-fit flex gap-2 items-center py-2 px-3 cursor-pointer rounded-full bg-violet-50 text-black",
-  "navbar": "text-white duration-300 ease-in hover:w-fit hover:flex hover:gap-2 hover:items-center hover:py-2 hover:px-3 hover:cursor-pointer hover:rounded-full hover:bg-violet-50 hover:text-black",
-  "clean": "",
+  "navbar": "py-2 px-3 text-white duration-300 ease-in hover:w-fit hover:flex hover:gap-2 hover:items-center hover:py-2 hover:px-3 hover:cursor-pointer hover:rounded-full hover:bg-violet-50 hover:text-black",
+  "clean": " ",
 }
 
 export function Button({ children, variant='default', className, ...props }:IPropsButton){
   return (
     <button
-      className={`h-8 ${variantButton[variant]}  ${className}`}
+      className={`h-8 ${variantButton[variant]}  ${className} `}
       {...props}
     >
         {children}
