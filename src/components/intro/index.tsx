658803bf064b3ed033c8f5091aa790d1c75370cc
Wellington <sousa_wellington@outlook.com>
2025-08-08 16:37:27 -0300

    feat: implement Intro component with GSAP animations and custom styles

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -2,6 +2,8 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
+import style from "./styles.module.css"
+import { AnimatedText } from "../ui/animatedText";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -55,62 +57,64 @@ export function Intro() {
   };
 
   useGSAP(() => {
-    // clip-path: ;
 
      gsap.set("#clip-image-child", {
-      clipPath: "polygon(25% 12%, 76% 16%, 83% 66%, 25% 80%)",
-    });
-    
-    
-    const clipAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#clip-image",
-        start: "center center",
-        end: "+=800 center", 
-        scrub: 0.5,
-        pin: true,
-        pinSpacing: true,
-      },
-    });
-    
+       clipPath: "polygon(25% 12%, 76% 16%, 83% 66%, 25% 80%)",
+      });
+      
+      const clipAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#clip-image",
+          start: "center center",
+          end: "+=800 center", 
+          scrub: 0.5,
+          pin: true,
+          pinSpacing: true,
+        },
+      });
+      
+  
     clipAnimation.to("#clip-image-child", {
-      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-      borderRadius: 0,
+      clipPath: "polygon(0.1% 0.1%, 100% 0.1%, 100% 100%, 0.1% 100%)",
+      webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      ease: "power1.inOut",
     });
   });
 
 
   return (
+
     <section className="flex items-center flex-col">
       <p className="tracking-wider font-semibold text-[10px] uppercase">
         Welcome to zentry
       </p>
 
       <div className="py-10">
-        <p className="text-center font-zentry text-6xl md:text-8xl">
-          Disc<span className="special-zentry">o</span>ver the world's
-        </p>
-        <p className="text-center font-zentry text-6xl md:text-8xl">
-          Largest shared <span className="special-zentry">A</span>dventures
-        </p>
+        <AnimatedText>
+            <p className="text-center font-zentry text-6xl md:text-8xl">
+              Disc<span className="special-zentry">o</span>ver the world's
+            </p>
+            <p className="text-center font-zentry text-6xl md:text-8xl">
+              Largest shared <span className="special-zentry">A</span>dventures
+            </p>
+        </AnimatedText>
       </div>
 
-
-      <div className="relative h-dvh border border-red-600 min-w-10"  id="clip-image">
+      <div className="relative h-dvh"  id="clip-image">
         <div
-          className="absolute z-10 left-1/2 h-dvh top-0 origin-center -translate-x-1/2 rounded-3xl" 
+          className={`${style.clipImageBorder} absolute z-10 left-1/2 top-0 h-dvh w-dvw origin-center -translate-x-1/2 rounded-3xl`}
           id="clip-image-child"
         >
           <img
             src="src/assets/images/custom-home-intro-desktop.webp"
             alt="Background"
-            className="absolute inset-0 size-full object-cover rounded-3xl"
+            className="absolute left-0 top-0 size-full object-cover rounded-3xl"
           />
         </div>
           <img
             src="src/assets/images/custom-home-intro.webp"
             alt="Background rocks"
-            className="absolute border-b-cyan-400 border-2 -translate-x-1/2 z-20 size-full min-w-fit object-cover"
+            className="absolute -translate-x-1/2 z-20 size-full min-w-fit object-cover"
             ref={imageCustomHomeIntroRef}
             onMouseLeave={handleMouseLeave}
             onMouseUp={handleMouseLeave}
