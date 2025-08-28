2615ecf5fa92c94b5450485028a831acd6ec3840
Wellington <sousa_wellington@outlook.com>
2025-08-28 20:15:24 -0300

    feat: implement GSAP animations and scroll interactions in ProductGrid component and correct UI in mobile screen

--- src/components/productGrid.tsx
+++ src/components/productGrid.tsx
@@ -1,9 +1,52 @@
 import { DivWithMouseInteractionEffects } from "./ui/divWithMouseInteractionEffects";
 import { GridItem } from "./ui/gridItem";
+import { useGSAP } from "@gsap/react";
+import { AnimatedText } from "./ui/animatedText";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export function PrductGrid (){
+  useGSAP(async() => {
+
+     // Aguarda fonts carregarem: minimalista e seguro!
+    if (document.fonts && document.fonts.ready) {
+      await document.fonts.ready;
+    }
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#top-information",
+        start: "center center",
+        end: "center center", 
+        scrub: true,
+        pin: true,
+      }
+    })
+
+    tl.set("#top-information",{
+      color: "#ffffff",
+      backgroundColor: "#09090b"
+    })
+    tl.set("#product-grid",{
+      color: "#ffffff",
+      backgroundColor: "#09090b"
+    })
+
+    tl.to("#top-information",{
+      color: "#09090b",
+      backgroundColor: "#EDFF66"
+    })
+    tl.to("#product-grid",{
+      color: "#09090b",
+      backgroundColor: "#EDFF66"
+    })
+  },  {revertOnUpdate: true });
+  
   return(
-    <section className="h-full text-white bg-zinc-950 font-medium text-[1.2rem] px-40 py-14">
+    <>
+    <section id="product-grid" className="h-full text-white bg-zinc-950 font-medium text-[1.2rem] px-40 py-14">
       <div className="py-36 leading-5 flex gap-1 flex-col">
         <p>Explore the Zentry Universe</p>
         <span className="text-gray-600 text-[1.15rem]">Immerse yourself in an IP-rich product universe where <br/>players, agentic AI and blockchain lead the new <br/>economic paradigm.</span>
@@ -12,22 +55,22 @@ export function PrductGrid (){
       {/* Product Grid item  */}
       <DivWithMouseInteractionEffects>
         <GridItem 
-          video={<video src="src/assets/video/radiant_desktop_homepage_v9.mp4" className="rounded-2xl w-full h-auto object-contain" loop muted/>}
+          video={<video src="src/assets/video/radiant_desktop_homepage_v9.mp4" className="rounded-2xl w-full h-auto object-contain " loop muted/>}
           title={<p className="mb-2 text-6xl font-zentry">RADIA<span className="special-zentry">N</span>T</p>}
           text={<p className="text-lg leading-5 text-zinc-300">The game of games app <br/> transforming moments across <br/>Web2 & Web3 titles into rewards</p>}
         />
       </DivWithMouseInteractionEffects>
 
-      <div className="grid grid-cols-2 gap-6 my-6">
+      <div className="grid lg:grid-cols-2 gap-6 my-6 grid-cols-1">
         <DivWithMouseInteractionEffects>
           <GridItem 
-            video={<video src="src/assets/video/zigma_desktop_homepage-lg.mp4" className="rounded-2xl w-full h-auto object-contain" loop muted/>}
+            video={<video src="src/assets/video/zigma_desktop_homepage-lg.mp4" className="rounded-2xl w-full h-auto object-cover aspect-square lg:object-contain lg:aspect-auto" loop muted/>}
             title={<p className="mb-2 text-6xl font-zentry">RADIA<span className="special-zentry">N</span>T</p>}
             text={<p className="text-lg leading-5 text-zinc-300">The game of games app <br/> transforming moments across <br/>Web2 & Web3 titles into rewards</p>}
           />
         </DivWithMouseInteractionEffects>
 
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between gap-6">
           <DivWithMouseInteractionEffects>
             <GridItem 
               video={<video src="src/assets/video/spectre_desktop_homepage-lg.mp4" className="rounded-2xl w-full h-auto object-contain" loop muted/>}
@@ -46,10 +89,10 @@ export function PrductGrid (){
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-6 my-6">
+      <div className="grid lg:grid-cols-2 gap-6 my-6 grid-cols-1">
         <DivWithMouseInteractionEffects>
           <GridItem
-            className="bg-[#5724ff] h-full" 
+            className="bg-[#5724ff] lg:h-full aspect-video max-w-full" 
             title={<p className="text-black mb-2 text-6xl font-zentry">M<span className="special-zentry">O</span>RE <br/> CO<span className="special-zentry">M</span>ING <br/> S<span className="special-zentry">OO</span>N.</p> }
             hiddenButton={true}
           />
@@ -63,5 +106,16 @@ export function PrductGrid (){
         </DivWithMouseInteractionEffects>
       </div>
     </section>
+    <section id="top-information" className="py-10 h-dvh">
+      <AnimatedText xAxis={-100} className="p-8 top-0">
+        <p className="text-7xl font-zentry">
+          the univ<span className="special-zentry">e</span>rse
+        </p>
+        <p className="text-7xl font-zentry">
+          powerd by ZE<span className="special-zentry">n</span>try
+        </p>
+      </AnimatedText>
+    </section>
+    </>
   )
 }
