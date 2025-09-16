7682d67ee296492f58c6b99577326bb006171ef1
Wellington <sousa_wellington@outlook.com>
2025-09-15 22:38:05 -0300

    refactor: clean up AccordionScroll component by removing unused props and simplifying span elements

--- src/components/ui/accordionScrool.tsx
+++ src/components/ui/accordionScrool.tsx
@@ -4,11 +4,6 @@ import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
 
-type Props = {
-  parentDuration: number;      // recebe el.duration do pai
-  multiplier?: number;         // controla a velocidade da animação
-};
-
 
 export function AccordionScrool({ totalDuration }: { totalDuration: number }) { 
   useGSAP(() => {
@@ -74,7 +69,7 @@ export function AccordionScrool({ totalDuration }: { totalDuration: number }) {
             <p className="text-sm">02</p>
             <p className="">Unlocking Economic Opportunity</p>
           </div>
-          <span className="hidden relative block ml-16 text-xs text-justify w-auto">
+          <span className="hidden relative ml-16 text-xs text-justify w-auto">
             ZENT is the index for crypto and AI, giving holders access to new markets, agent tokenization, data economies, and protocol rewards, unlocking utility and upside across the industries it powers.
             <div className="w-0.5 bg-black h-full absolute bottom-0 -left-[58px]"></div>
             <div className="progress w-0.5 bg-zinc-400 h-full absolute bottom-0 -left-[58px]"></div>
@@ -86,7 +81,7 @@ export function AccordionScrool({ totalDuration }: { totalDuration: number }) {
             <p className="text-sm">03</p>
             <p className="">Sharing Value Accrued</p>
           </div>
-          <span className="hidden relative block ml-16 text-xs text-justify w-auto">
+          <span className="hidden relative ml-16 text-xs text-justify w-auto">
             ZENT holders benefit from Zentry’s ecosystem growth, capturing value from partnerships, treasury strategy, and real-world participation.
             <div className="w-0.5 bg-black h-full absolute bottom-0 -left-[58px]"></div>
             <div className="progress w-0.5 bg-zinc-400 h-full absolute bottom-0 -left-[58px]"></div>
